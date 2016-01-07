5a8a38079de9f5747ae6f2b56d2f67b2ce1ac19b
malex <mircea.alexandru@gmail.com>
2016-01-07 15:07:30 +0200

    added tests

--- test/urlmatcher.test.js
+++ test/urlmatcher.test.js
@@ -4,18 +4,49 @@ var Lab = require('lab')
 var lab = exports.lab = Lab.script()
 var suite = lab.suite
 var test = lab.test
-var before = lab.before
 
-var Util = require('./util.js')
+var Code = require('code')
+var expect = Code.expect
 
-suite('register-login-logout suite tests ', function () {
-  before({}, function (done) {
-    Util.init(function (err, agentData) {
-      done(err)
+suite('urlmatcher suite tests', function () {
+  test('simple matcher', function (done) {
+    var si = require('seneca')()
+
+    si.use(require('..'))
+    si.act('role: auth, cmd: urlmatcher', {spec: '/api'}, function (err, result) {
+      expect(err).to.not.exist()
+      expect(result).to.be.array()
+      expect(result).to.have.length(1)
+      expect(result[0]).to.be.function()
+
+      done()
     })
   })
+  test('array matcher', function (done) {
+    var si = require('seneca')()
 
-  test('match test', function (done) {
-    done()
+    si.use(require('..'))
+    si.act('role: auth, cmd: urlmatcher', {spec: ['/api', '/api2']}, function (err, result) {
+      expect(err).to.not.exist()
+      expect(result).to.be.array()
+      expect(result).to.have.length(2)
+      expect(result[0]).to.be.function()
+      expect(result[1]).to.be.function()
+
+      done()
+    })
+  })
+  test('function matcher', function (done) {
+    var si = require('seneca')()
+
+    si.use(require('..'))
+    si.act('role: auth, cmd: urlmatcher', {spec: function () {}}, function (err, result) {
+      expect(err).to.not.exist()
+      expect(result).to.be.array()
+      expect(result).to.have.length(1)
+      expect(result[0]).to.be.function()
+
+      done()
+    })
   })
 })
